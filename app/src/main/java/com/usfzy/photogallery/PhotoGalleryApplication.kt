c7e55dd2bc4f58c80b692aaaf39895247b9a138b
Usfzy <usfzy47@gmail.com>
2024-01-10 21:55:19 +0000

    added Notifications

--- app/src/main/java/com/usfzy/photogallery/PhotoGalleryApplication.kt
+++ app/src/main/java/com/usfzy/photogallery/PhotoGalleryApplication.kt
@@ -1,12 +1,27 @@
 package com.usfzy.photogallery
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.usfzy.photogallery.repository.PreferenceRepository
+
+const val NOTIFICATION_CHANNEL_ID = "flicker_poll"
 
 class PhotoGalleryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         PreferenceRepository.initialize(this)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
+            val notificationManager: NotificationManager =
+                getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/usfzy/photogallery/worker/PollWorker.kt
+++ app/src/main/java/com/usfzy/photogallery/worker/PollWorker.kt
@@ -1,9 +1,18 @@
 package com.usfzy.photogallery.worker
 
+import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.usfzy.photogallery.MainActivity
+import com.usfzy.photogallery.NOTIFICATION_CHANNEL_ID
+import com.usfzy.photogallery.R
 import com.usfzy.photogallery.repository.PhotoRepository
 import com.usfzy.photogallery.repository.PreferenceRepository
 import kotlinx.coroutines.flow.first
@@ -11,7 +20,7 @@ import kotlinx.coroutines.flow.first
 private const val TAG = "PollWorker"
 
 class PollWorker(
-    context: Context,
+    private val context: Context,
     workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
 
@@ -39,6 +48,7 @@ class PollWorker(
                 } else {
                     Log.d(TAG, "doWork: Got a new Result")
                     preferenceRepository.setLastResultId(newResId)
+                    notifyUser()
                 }
             }
 
@@ -47,5 +57,31 @@ class PollWorker(
             Log.d(TAG, "doWork: ${ex.message}")
             Result.success()
         }
+    }
+
+    private fun notifyUser() {
+        val intent = MainActivity.newIntent(context)
+        val pendingIntent = PendingIntent.getActivity(
+            context, 0, intent, PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val resources = context.resources
+        val notification = NotificationCompat
+            .Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setTicker(resources.getString(R.string.new_pictures_title))
+            .setSmallIcon(android.R.drawable.ic_menu_report_image)
+            .setContentTitle(resources.getString(R.string.new_pictures_title))
+            .setContentText(resources.getString(R.string.new_pictures_text))
+            .setContentIntent(pendingIntent) // fired when users clicks on notification
+            .setAutoCancel(true)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        NotificationManagerCompat.from(context).notify(0, notification)
     }
 }
